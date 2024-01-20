043ea1f79d3af361d3a53348266b01ae8dfa563e
Sinisterchilll <72158537+Sinisterchilll@users.noreply.github.com>
2024-01-20 12:09:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 async function start() {
-    //await readTodos();
+    await readTodos();
     app.listen(port, () => {
         console.log("Server is running");
     });
